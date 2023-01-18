f334b9efaef1c9d0027d1f746bf4999cbb8690d5
Star Shadow <starshadow@Stars-MacBook-Pro.local>
2023-01-18 20:58:22 +0530

    Patch Req Body recieved at the endpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const ejs = require('ejs');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const axios = require('axios');
 
 // ---------------- Initializing Node Express App ---------------- 
 const app = express();
@@ -13,6 +14,7 @@ mongoose.connect("mongodb://localhost:27017/manabazaarTest", {useNewUrlParser : 
 // ---------------- Configs ---------------- 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // ---------------- Intializing Inventory Schema ---------------- 
@@ -145,6 +147,7 @@ app.route("/Inventory/:Category")
     // ---------------- UPDATE ----------------
     .patch((req, res) => {
         console.log("Patch route is hit successfully");
+        console.log(req.body);
     })
 
 
